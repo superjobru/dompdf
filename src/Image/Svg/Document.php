d549daeb8dd97f299b2977509509894e1538d053
Дарья Сергеева <d.sergeeva@superjob.ru>
2019-03-29 15:54:19 +0300

    Fix problem with parsing svg image tag

---
+++ src/Image/Svg/Document.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dompdf\Image\Svg;
+
+use \Svg\Document as SvgDocument;
+
+class Document extends SvgDocument
+{
+    public function __construct()
+    {
+        $this->attributes = [];
+        parent::construct();
+    }
+}
